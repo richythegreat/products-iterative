51bbbbc1fa9bb0c06b53c28bae200d6e9956d508
RicardsAlmanis <you@example.com>
2025-10-31 15:35:43 +0200

    added '/' skip to'/products'

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/products');
+// Produkta daudzuma maiņas maršruti
+Route::post('/products/{product}/increase', [ProductController::class, 'increaseQuantity'])->name('products.increase');
+Route::post('/products/{product}/decrease', [ProductController::class, 'decreaseQuantity'])->name('products.decrease');
 
 Route::resource('products', ProductController::class);
