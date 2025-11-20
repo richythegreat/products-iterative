e15a2aeb204c7d8f1a54be1790e497e4d4c75668
You <you@example.com>
2025-11-20 15:21:27 +0200

    added tag routes

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,13 @@ Route::redirect('/', '/products');
 // Produkta daudzuma maiņas maršruti
 Route::post('/products/{product}/increase', [ProductController::class, 'increaseQuantity'])->name('products.increase');
 Route::post('/products/{product}/decrease', [ProductController::class, 'decreaseQuantity'])->name('products.decrease');
+// Autocomplete
+Route::get('/tags/search', [ProductController::class, 'searchTags'])
+    ->name('tags.search');
+
+// Update tags with PUT
+Route::put('/products/{product}/update-tags', [ProductController::class, 'updateTags'])
+    ->name('products.updateTags');
+
 
 Route::resource('products', ProductController::class);
